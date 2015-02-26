61a93e06c12fca27331b7681e138a8589ee41b0f
ayanuali <ayanuali@gmail.com>
2015-02-26 21:27:56 +0600

    added some before actions

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150219045331) do
+ActiveRecord::Schema.define(version: 20150226151703) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
